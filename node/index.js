05e18044a42d22f408657ec3f0c3a278f986b97f
Paolo Bernasconi <pbernasconi@users.noreply.github.com>
2017-04-11 16:32:37 -0700

    Cleanup nodejs example (#6)

--- node/index.js
+++ node/index.js
@@ -19,106 +19,122 @@ var PUBLIC_TOKEN = null;
 
 // Initialize the Plaid client
 var client = new plaid.Client(
-    PLAID_CLIENT_ID,
-    PLAID_SECRET,
-    PLAID_PUBLIC_KEY,
-    plaid.environments[PLAID_ENV]
+  PLAID_CLIENT_ID,
+  PLAID_SECRET,
+  PLAID_PUBLIC_KEY,
+  plaid.environments[PLAID_ENV]
 );
 
 var app = express();
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(bodyParser.json());
 
 app.get('/', function(request, response, next) {
-    response.render('index.ejs', {
-        PLAID_PUBLIC_KEY: PLAID_PUBLIC_KEY,
-        PLAID_ENV: PLAID_ENV,
-    });
+  response.render('index.ejs', {
+    PLAID_PUBLIC_KEY: PLAID_PUBLIC_KEY,
+    PLAID_ENV: PLAID_ENV,
+  });
 });
 
 app.post('/get_access_token', function(request, response, next) {
-    PUBLIC_TOKEN = request.body.public_token;
-    client.exchangePublicToken(PUBLIC_TOKEN, function(error, tokenResponse) {
-        if (error != null) {
-            var msg = 'Could not exchange public_token!';
-            console.log(msg + '\n' + error);
-            return response.json({error: msg});
-        }
-        ACCESS_TOKEN = tokenResponse.access_token;
-        console.log('Access Token: ' + ACCESS_TOKEN);
-        response.json({'error': false});
+  PUBLIC_TOKEN = request.body.public_token;
+  client.exchangePublicToken(PUBLIC_TOKEN, function(error, tokenResponse) {
+    if (error != null) {
+      var msg = 'Could not exchange public_token!';
+      console.log(msg + '\n' + error);
+      return response.json({
+        error: msg
+      });
+    }
+    ACCESS_TOKEN = tokenResponse.access_token;
+    console.log('Access Token: ' + ACCESS_TOKEN);
+    response.json({
+      'error': false
     });
+  });
 });
 
 app.post('/set_access_token', function(request, response, next) {
-    ACCESS_TOKEN = request.body.access_token;
-    console.log('Access Token: ' + ACCESS_TOKEN);
-    response.json({'error': false});
+  ACCESS_TOKEN = request.body.access_token;
+  console.log('Access Token: ' + ACCESS_TOKEN);
+  response.json({
+    'error': false
+  });
 });
 
 app.get('/accounts', function(request, response, next) {
-    // Retrieve high-level account information and account and routing numbers
-    // for each account associated with the Item.
-    client.getAuth(ACCESS_TOKEN, function(error, authResponse) {
-        if(error != null) {
-            var msg = 'Unable to pull accounts from the Plaid API.';
-            console.log(msg + '\n' + error);
-            return response.json({error: msg});
-        }
+  // Retrieve high-level account information and account and routing numbers
+  // for each account associated with the Item.
+  client.getAuth(ACCESS_TOKEN, function(error, authResponse) {
+    if (error != null) {
+      var msg = 'Unable to pull accounts from the Plaid API.';
+      console.log(msg + '\n' + error);
+      return response.json({
+        error: msg
+      });
+    }
 
-        console.log(authResponse.accounts);
-        response.json({
-            error: false,
-            accounts: authResponse.accounts,
-            numbers: authResponse.numbers,
-        });
+    console.log(authResponse.accounts);
+    response.json({
+      error: false,
+      accounts: authResponse.accounts,
+      numbers: authResponse.numbers,
     });
+  });
 });
 
 app.post('/item', function(request, response, next) {
-    // Pull the Item - this includes information about available products,
-    // billed products, webhook information, and more.
-    client.getItem(ACCESS_TOKEN, function(error, itemResponse) {
-        if (error != null) {
-            console.log(JSON.stringify(error));
-            return response.json({error: error});
-        }
+  // Pull the Item - this includes information about available products,
+  // billed products, webhook information, and more.
+  client.getItem(ACCESS_TOKEN, function(error, itemResponse) {
+    if (error != null) {
+      console.log(JSON.stringify(error));
+      return response.json({
+        error: error
+      });
+    }
 
-        // Also pull information about the institution
-        client.getInstitutionById(itemResponse.item.institution_id, function(err, instRes) {
-            if (err != null) {
-            var msg = 'Unable to pull institution information from the Plaid API.';
-            console.log(msg + '\n' + error);
-            return response.json({error: msg});
-            } else {
-                response.json({
-                    item: itemResponse.item,
-                    institution: instRes.institution,
-                });
-            }
+    // Also pull information about the institution
+    client.getInstitutionById(itemResponse.item.institution_id, function(err, instRes) {
+      if (err != null) {
+        var msg = 'Unable to pull institution information from the Plaid API.';
+        console.log(msg + '\n' + error);
+        return response.json({
+          error: msg
         });
+      } else {
+        response.json({
+          item: itemResponse.item,
+          institution: instRes.institution,
+        });
+      }
     });
+  });
 });
 
 app.post('/transactions', function(request, response, next) {
-    // Pull transactions for the Item for the last 30 days
-    var startDate = moment().subtract(30, 'days').format('YYYY-MM-DD');
-    var endDate = moment().format('YYYY-MM-DD');
-    client.getTransactions(ACCESS_TOKEN, startDate, endDate, {
-        count: 250,
-        offset: 0,
-    }, function(error, transactionsResponse) {
-        if (error != null) {
-            console.log(JSON.stringify(error));
-            return response.json({error: error});
-        }
-        console.log('pulled ' + transactionsResponse.transactions.length + ' transactions');
-        response.json(transactionsResponse);
-    });
+  // Pull transactions for the Item for the last 30 days
+  var startDate = moment().subtract(30, 'days').format('YYYY-MM-DD');
+  var endDate = moment().format('YYYY-MM-DD');
+  client.getTransactions(ACCESS_TOKEN, startDate, endDate, {
+    count: 250,
+    offset: 0,
+  }, function(error, transactionsResponse) {
+    if (error != null) {
+      console.log(JSON.stringify(error));
+      return response.json({
+        error: error
+      });
+    }
+    console.log('pulled ' + transactionsResponse.transactions.length + ' transactions');
+    response.json(transactionsResponse);
+  });
 });
 
-var server = app.listen(APP_PORT, function () {
-    console.log('plaid-walkthrough server listening on port ' + APP_PORT);
+var server = app.listen(APP_PORT, function() {
+  console.log('plaid-walkthrough server listening on port ' + APP_PORT);
 });
