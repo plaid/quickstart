bdc96b1d383cdc51ff5d3f5bb7b305f48f70837e
Jack Siegel <jack@plaid.com>
2017-06-14 16:01:48 -0700

    add Ruby exception handling for transactions request

--- ruby/app.rb
+++ ruby/app.rb
@@ -40,7 +40,11 @@ end
 get '/transactions' do
   now = Date.today
   thirty_days_ago = (now - 30)
-  transactions_response = client.transactions.get(access_token, thirty_days_ago, now)
+  begin
+    transactions_response = client.transactions.get(access_token, thirty_days_ago, now)
+  rescue Plaid::ItemError => e
+    transactions_response = { error: {error_code: e.error_code, error_message: e.error_message}}
+  end
   content_type :json
   transactions_response.to_json
 end
