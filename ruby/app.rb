c4dd52c85762d68cf16c867a7dbf43a4724de8dd
Michael Kelly <michael@plaid.com>
2018-09-21 09:05:27 -0700

    update ruby

--- ruby/app.rb
+++ ruby/app.rb
@@ -17,6 +17,9 @@ get '/' do
   erb :index
 end
 
+# Exchange token flow - exchange a Link public_token for
+# an API access_token
+# https://plaid.com/docs/#exchange-token-flow
 post '/get_access_token' do
   exchange_token_response =
     client.item.public_token.exchange(params['public_token'])
@@ -27,50 +30,92 @@ post '/get_access_token' do
   exchange_token_response.to_json
 end
 
-post '/get_product' do
-  product_response = nil
-  error_response = nil
-  if params['product'] == 'transactions'
-    now = Date.today
-    thirty_days_ago = (now - 30)
-    begin
-      product_response =
-        client.transactions.get(access_token, thirty_days_ago, now)
-    rescue Plaid::PlaidAPIError => e
-      error_response = format_error(e)
-    end
-  elsif params['product'] == 'auth'
-    begin
-      product_response = client.auth.get(access_token)
-    rescue Plaid::PlaidAPIError => e
-      error_response = format_error(e)
-    end
-  elsif params['product'] == 'identity'
-    begin
-      product_response = client.identity.get(access_token)
-    rescue Plaid::PlaidAPIError => e
-      error_response = format_error(e)
-    end
-  elsif params['product'] == 'balance'
-    begin
-      product_response = client.accounts.balance.get(access_token)
-    rescue Plaid::PlaidAPIError => e
-      error_response = format_error(e)
-    end
-  end
-
-  if !error_response.nil?
+# Retrieve Transactions for an Item
+# https://plaid.com/docs/#transactions
+get '/transactions' do
+  now = Date.today
+  thirty_days_ago = (now - 30)
+  begin
+    product_response =
+      client.transactions.get(access_token, thirty_days_ago, now)
+    pretty_print_response(product_response)
+    content_type :json
+    { transactions => product_response }.to_json
+  rescue Plaid::PlaidAPIError => e
+    error_response = format_error(e)
     pretty_print_response(error_response)
     content_type :json
     error_response.to_json
-  else
-    pretty_print_response(product_response)
-    content_type :json
-    { params['product'] => product_response }.to_json
   end
 end
 
-post '/item' do
+# Retrieve ACH or ETF account numbers for an Item
+# https://plaid.com/docs/#auth
+get '/auth' do
+  begin
+    product_response = client.auth.get(access_token)
+    pretty_print_response(product_response)
+    content_type :json
+    { auth: product_response }.to_json
+  rescue Plaid::PlaidAPIError => e
+    error_response = format_error(e)
+    pretty_print_response(error_response)
+    content_type :json
+    error_response.to_json
+  end
+end
+
+# Retrieve Identity data for an Item
+# https://plaid.com/docs/#identity
+get '/identity' do
+  begin
+    product_response = client.identity.get(access_token)
+    pretty_print_response(product_response)
+    content_type :json
+    { identity: product_response }.to_json
+  rescue Plaid::PlaidAPIError => e
+    error_response = format_error(e)
+    pretty_print_response(error_response)
+    content_type :json
+    error_response.to_json
+  end
+end
+
+# Retrieve real-time balance data for each of an Item's accounts
+# https://plaid.com/docs/#balance
+get '/balance' do
+  begin
+    product_response = client.accounts.balance.get(access_token)
+    pretty_print_response(product_response)
+    content_type :json
+    { balance: product_response }.to_json
+  rescue Plaid::PlaidAPIError => e
+    error_response = format_error(e)
+    pretty_print_response(error_response)
+    content_type :json
+    error_response.to_json
+  end
+end
+
+# Retrieve an Item's accounts
+# https://plaid.com/docs/#accounts
+get '/accounts' do
+  begin
+    product_response = client.accounts.get(access_token)
+    pretty_print_response(product_response)
+    content_type :json
+    { accounts: product_response }.to_json
+  rescue Plaid::PlaidAPIError => e
+    error_response = format_error(e)
+    pretty_print_response(error_response)
+    content_type :json
+    error_response.to_json
+  end
+end
+
+# Retrieve high-level information about an Item
+# https://plaid.com/docs/#retrieve-item
+get '/item' do
   item_response = client.item.get(access_token)
   institution_response =
     client.institutions.get_by_id(item_response['item']['institution_id'])
