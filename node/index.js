859e8b03b8488108cb99a26adb06ed0942824f7f
Ari Gibson <arimgibson@gmail.com>
2020-10-01 01:01:55 -0600

    Added .split

--- node/index.js
+++ node/index.js
@@ -15,11 +15,11 @@ var PLAID_ENV = process.env.PLAID_ENV || 'sandbox';
 // PLAID_PRODUCTS is a comma-separated list of products to use when initializing
 // Link. Note that this list must contain 'assets' in order for the app to be
 // able to create and retrieve asset reports.
-var PLAID_PRODUCTS = process.env.PLAID_PRODUCTS || 'transactions';
+var PLAID_PRODUCTS = process.env.PLAID_PRODUCTS.split(',') || 'transactions';
 
 // PLAID_PRODUCTS is a comma-separated list of countries for which users
 // will be able to select institutions from.
-var PLAID_COUNTRY_CODES = process.env.PLAID_COUNTRY_CODES || 'US';
+var PLAID_COUNTRY_CODES = process.env.PLAID_COUNTRY_CODES.split(',') || 'US';
 
 // Parameters used for the OAuth redirect Link flow.
 //
