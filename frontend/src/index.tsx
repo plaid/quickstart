5f24d22696c75357cb9d93de3013dac4fc3ae265
Linda <lwoo@plaid.com>
2021-01-21 09:19:35 -0800

    delete excess react-create-app files

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import styles from "./index.module.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
