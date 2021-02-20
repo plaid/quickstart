52c45c1f815200a3b24d3fd4f6f413e451b7f9af
= <gateswang00@gmail.com>
2021-02-20 15:20:41 -0500

    removed items and only product remaining is transactions

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -45,7 +45,6 @@ const App = () => {
         {linkSuccess && isItemAccess && (
           <>
             <Products />
-            <Items />
           </>
         )}
       </div>
--- frontend/src/Components/ProductTypes/Products.tsx
+++ frontend/src/Components/ProductTypes/Products.tsx
@@ -19,48 +19,12 @@ const Products = () => {
   return (
     <ProductTypesContainer productType="Products">
       <Endpoint
-        endpoint="auth"
-        name="Auth"
-        categories={authCategories}
-        schema="/auth/get/"
-        description="Retrieve account and routing numbers for checking and savings accounts."
-        transformData={transformAuthData}
-      />
-      <Endpoint
         endpoint="transactions"
         name="Transactions"
         categories={transactionsCategories}
         schema="/transactions/get/"
         description="Retrieve transactions for credit and depository accounts."
         transformData={transformTransactionsData}
-      />
-      <Endpoint
-        endpoint="identity"
-        name="Identity"
-        categories={identityCategories}
-        schema="/identity/get/"
-        description="Retrieve Identity information on file with the bank. Reduce
-        fraud by comparing user-submitted data to validate identity."
-        transformData={transformIdentityData}
-      />
-      <Endpoint
-        endpoint="balance"
-        name="Balance"
-        categories={balanceCategories}
-        schema="/accounts/balance/get/"
-        description="Check balances in real time to prevent non-sufficient funds
-        fees."
-        transformData={transformBalanceData}
-      />
-      <Endpoint
-        endpoint="holdings"
-        name="Investments"
-        categories={investmentsCategories}
-        schema="/investments/holdings/get/"
-        description="Retrieve investment holdings on file with the bank,
-        brokerage, or investment institution. Analyze over-exposure
-        to market segments."
-        transformData={transformInvestmentsData}
       />
     </ProductTypesContainer>
   );
