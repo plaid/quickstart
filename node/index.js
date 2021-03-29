ca2686034b2e0a11fdb99465c6a9dbd4896191c8
Linda <lwoo@plaid.com>
2021-03-29 16:04:21 -0700

    fix pdf buffer

--- node/index.js
+++ node/index.js
@@ -391,9 +391,10 @@ app.get('/api/assets', async function (request, response, next) {
     });
     prettyPrintResponse(getResponse);
     prettyPrintResponse(pdfResponse);
+    prettyPrintResponse(pdfResponse);
     response.json({
       json: getResponse.data.report,
-      pdf: pdfResponse.data.buffer.toString('base64'),
+      pdf: pdfResponse.data.toString('base64'),
     });
   } catch {
     prettyPrintResponse(error);
