6b9663cbcd393b0949be9090b58659171e722721
Linda <lwoo@plaid.com>
2021-04-14 10:09:27 -0700

    standard syntax for hashes and rewrite asset report endpoint

--- ruby/app.rb
+++ ruby/app.rb
@@ -7,7 +7,7 @@ require 'date'
 require 'json'
 require 'plaid'
 require 'sinatra'
-require 'pp'
+# require 'pp'
 
 set :port, ENV['APP_PORT'] || 8000
 
@@ -50,11 +50,12 @@ end
 # https://plaid.com/docs/#exchange-token-flow
 post '/api/set_access_token' do
   item_public_token_exchange_request = Plaid::ItemPublicTokenExchangeRequest.new(
-    {:public_token=>params["public_token"]})
-  exchange_token_response =
-  client.item_public_token_exchange(
-    item_public_token_exchange_request
+    { public_token: params['public_token'] }
   )
+  exchange_token_response =
+    client.item_public_token_exchange(
+      item_public_token_exchange_request
+    )
   access_token = exchange_token_response.access_token
   item_id = exchange_token_response.item_id
   pretty_print_response(exchange_token_response)
@@ -65,13 +66,16 @@ end
 # Retrieve Transactions for an Item
 # https://plaid.com/docs/#transactions
 get '/api/transactions' do
-  START_DATE = (Date.today - 365)
-  END_DATE = Date.today
   begin
-    transactions_get_request = Plaid::TransactionsGetRequest.new
-    transactions_get_request.access_token = access_token
-    transactions_get_request.start_date = START_DATE
-    transactions_get_request.end_date = END_DATE
+    START_DATE = (Date.today - 365)
+    END_DATE = Date.today
+    transactions_get_request = Plaid::TransactionsGetRequest.new(
+      {
+        access_token: access_token,
+        start_date: START_DATE,
+        end_date: END_DATE
+      }
+    )
     transactions_response =
       client.transactions_get(transactions_get_request)
     pretty_print_response(transactions_response)
@@ -89,8 +93,7 @@ end
 # https://plaid.com/docs/#auth
 get '/api/auth' do
   begin
-    auth_get_request = Plaid::AuthGetRequest.new
-    auth_get_request.access_token = access_token
+    auth_get_request = Plaid::AuthGetRequest.new({ access_token: access_token })
     auth_response = client.auth_get(auth_get_request)
     pretty_print_response(auth_response)
     content_type :json
@@ -107,8 +110,7 @@ end
 # https://plaid.com/docs/#identity
 get '/api/identity' do
   begin
-    identity_get_request = Plaid::IdentityGetRequest.new
-    identity_get_request.access_token = access_token
+    identity_get_request = Plaid::IdentityGetRequest.new({ access_token: access_token })
     identity_response = client.identity_get(identity_get_request)
     pretty_print_response(identity_response)
     content_type :json
@@ -125,8 +127,7 @@ end
 # https://plaid.com/docs/#balance
 get '/api/balance' do
   begin
-    balance_get_request = Plaid::AccountsBalanceGetRequest.new
-    balance_get_request.access_token = access_token
+    balance_get_request = Plaid::AccountsBalanceGetRequest.new({ access_token: access_token })
     balance_response = client.accounts_balance_get(balance_get_request)
     pretty_print_response(balance_response)
     content_type :json
@@ -143,8 +144,7 @@ end
 # https://plaid.com/docs/#accounts
 get '/api/accounts' do
   begin
-    accounts_get_request = Plaid::AccountsGetRequest.new
-    accounts_get_request.access_token = access_token
+    accounts_get_request = Plaid::AccountsGetRequest.new({ access_token: access_token })
     account_response = client.accounts_get(accounts_get_request)
     pretty_print_response(account_response)
     content_type :json
@@ -161,8 +161,7 @@ end
 # https://plaid.com/docs/#investments
 get '/api/holdings' do
   begin
-    investments_holdings_get_request = Plaid::InvestmentsHoldingsGetRequest.new
-    investments_holdings_get_request.access_token = access_token
+    investments_holdings_get_request = Plaid::InvestmentsHoldingsGetRequest.new({ access_token: access_token })
     product_response = client.investments_holdings_get(investments_holdings_get_request)
     pretty_print_response(product_response)
     content_type :json
@@ -178,13 +177,16 @@ end
 # Retrieve Investment Transactions for an Item
 # https://plaid.com/docs/#investments
 get '/api/investment_transactions' do
-  START_DATE = (Date.today - 365)
-  END_DATE = Date.today
   begin
-    investments_transactions_get_request = Plaid::InvestmentsTransactionsGetRequest.new
-    investments_transactions_get_request.access_token = access_token
-    investments_transactions_get_request.start_date = START_DATE
-    investments_transactions_get_request.end_date = END_DATE
+    START_DATE = (Date.today - 365)
+    END_DATE = Date.today
+    investments_transactions_get_request = Plaid::InvestmentsTransactionsGetRequest.new(
+      {
+        access_token: access_token,
+        start_date: START_DATE,
+        end_date: END_DATE
+      }
+    )
     response = client.investments_transactions_get(investments_transactions_get_request)
     pretty_print_response(response)
     content_type :json
@@ -217,10 +219,13 @@ get '/api/assets' do
         email: 'jane.doe@example.com'
       }
     }
-    asset_report_create_request = Plaid::AssetReportCreateRequest.new
-    asset_report_create_request.access_tokens = [access_token]
-    asset_report_create_request.days_requested = 20
-    asset_report_create_request.options = options
+    asset_report_create_request = Plaid::AssetReportCreateRequest.new(
+      {
+        access_tokens: [access_token],
+        days_requested: 20,
+        options: options
+      }
+    )
     asset_report_create_response =
       client.asset_report_create(asset_report_create_request)
     pretty_print_response(asset_report_create_response)
@@ -233,27 +238,25 @@ get '/api/assets' do
 
   asset_report_token = asset_report_create_response.asset_report_token
   asset_report_json = nil
-  asset_report_pdf = nil
-
-  begin
-    50.times do
-      begin
-        asset_report_get_request = Plaid::AssetReportGetRequest.new
-        asset_report_get_request.asset_report_token = asset_report_token
-        asset_report_get_response = client.asset_report_get(asset_report_get_request)
-        asset_report_json = asset_report_get_response.report
-        asset_report_pdf_get_request = Plaid::AssetReportPDFGetRequest.new
-        asset_report_pdf_get_request.asset_report_token = asset_report_token
-        asset_report_pdf = client.asset_report_pdf_get( asset_report_pdf_get_request)
-        break
-      rescue Plaid::ApiError => e
-        json_response = JSON.parse(e.response_body)
-        raise e if json_response['error_code'] != 'PRODUCT_NOT_READY'
-
-        sleep 1
+  num_retries_remaining = 20
+  while num_retries_remaining.positive?
+    begin
+      asset_report_get_request = Plaid::AssetReportGetRequest.new({ asset_report_token: asset_report_token })
+      asset_report_get_response = client.asset_report_get(asset_report_get_request)
+      asset_report_json = asset_report_get_response.report
+      break
+    rescue Plaid::ApiError => e
+      json_response = JSON.parse(e.response_body)
+      if json_response['error_code'] == 'PRODUCT_NOT_READY'
+        num_retries_remaining -= 1
+        sleep(1)
+        next
       end
+      error_response = format_error(e)
+      pretty_print_response(error_response)
+      content_type :json
+      return error_response.to_json
     end
-  
   end
 
   if asset_report_json.nil?
@@ -266,6 +269,9 @@ get '/api/assets' do
     }.to_json
   end
 
+  asset_report_pdf_get_request = Plaid::AssetReportPDFGetRequest.new({ asset_report_token: asset_report_token })
+  asset_report_pdf = client.asset_report_pdf_get( asset_report_pdf_get_request)
+
   content_type :json
   { json: asset_report_json.to_hash,
     pdf: Base64.encode64(File.read(asset_report_pdf)) }.to_json
@@ -277,12 +283,14 @@ end
 # https://plaid.com/docs/#retrieve-item
 get '/api/item' do
   begin
-    item_get_request = Plaid::ItemGetRequest.new
-    item_get_request.access_token = access_token
+    item_get_request = Plaid::ItemGetRequest.new({ access_token: access_token})
     item_response = client.item_get(item_get_request)
-    institutions_get_by_id_request = Plaid::InstitutionsGetByIdRequest.new
-    institutions_get_by_id_request.institution_id = item_response.item.institution_id
-    institutions_get_by_id_request.country_codes = ['US']
+    institutions_get_by_id_request = Plaid::InstitutionsGetByIdRequest.new(
+      {
+        institution_id: item_response.item.institution_id,
+        country_codes: ['US']
+      }
+    )
     institution_response =
       client.institutions_get_by_id(institutions_get_by_id_request)
     pretty_print_response(item_response)
@@ -302,8 +310,7 @@ end
 # Retrieve Payment for a specified Payment ID
 get '/api/payment' do
   begin
-    payment_initiation_payment_get_request = Plaid::PaymentInitiationPaymentGetRequest.new
-    payment_initiation_payment_get_request.payment_id = payment_id
+    payment_initiation_payment_get_request = Plaid::PaymentInitiationPaymentGetRequest.new({ payment_id: payment_id})
     payment_get_response = client.payment_initiation_payment_get(payment_initiation_payment_get_request)
     pretty_print_response(payment_get_response)
     content_type :json
@@ -320,12 +327,12 @@ post '/api/create_link_token' do
   begin
     link_token_create_request = Plaid::LinkTokenCreateRequest.new(
       {
-        :user => { :client_user_id => 'user-id' },
-        :client_name => 'Plaid Quickstart',
-        :products => ENV['PLAID_PRODUCTS'].split(','),
-        :country_codes => ENV['PLAID_COUNTRY_CODES'].split(','),
-        :language => "en",
-        :redirect_uri => nil_if_empty_envvar('PLAID_REDIRECT_URI')
+        user: { client_user_id: 'user-id' },
+        client_name: 'Plaid Quickstart',
+        products: ENV['PLAID_PRODUCTS'].split(','),
+        country_codes: ENV['PLAID_COUNTRY_CODES'].split(','),
+        language: 'en',
+        redirect_uri: nil_if_empty_envvar('PLAID_REDIRECT_URI')
       }
     )
     response = client.link_token_create(link_token_create_request)
@@ -356,52 +363,62 @@ end
 # This makes the quickstart easier to use.
 post '/api/create_link_token_for_payment' do
   begin
-    payment_initiation_recipient_create_request = Plaid::PaymentInitiationRecipientCreateRequest.new
-    payment_initiation_recipient_create_request.name = 'Bruce Wayne'
-    payment_initiation_recipient_create_request.iban = 'GB33BUKB20201555555555'
-    payment_initiation_recipient_create_request.address = {
-      street: ['686 Bat Cave Lane'],
-      city: 'Gotham',
-      postal_code: '99999',
-      country: 'GB',
-    }
-    payment_initiation_recipient_create_request.bacs = {
-      account: '26207729',
-      sort_code: '560029',
-    }
-
+    payment_initiation_recipient_create_request = Plaid::PaymentInitiationRecipientCreateRequest.new(
+      {
+        name: 'Bruce Wayne',
+        iban: 'GB33BUKB20201555555555',
+        address: {
+          street: ['686 Bat Cave Lane'],
+          city: 'Gotham',
+          postal_code: '99999',
+          country: 'GB',
+        },
+        bacs: {
+          account: '26207729',
+          sort_code: '560029',
+        }
+      }
+    )
     create_recipient_response = client.payment_initiation_recipient_create(
       payment_initiation_recipient_create_request
     )
     recipient_id = create_recipient_response.recipient_id
 
-    payment_initiation_recipient_get_request = Plaid::PaymentInitiationRecipientGetRequest.new
-    payment_initiation_recipient_get_request.recipient_id = recipient_id
+    payment_initiation_recipient_get_request = Plaid::PaymentInitiationRecipientGetRequest.new(
+      {
+        recipient_id: recipient_id
+      }
+    )
     get_recipient_response = client.payment_initiation_recipient_get(
       payment_initiation_recipient_get_request
     )
 
-    payment_initiation_payment_create_request = Plaid::PaymentInitiationPaymentCreateRequest.new
-    payment_initiation_payment_create_request.recipient_id = recipient_id
-    payment_initiation_payment_create_request.reference = 'testpayment'
-    payment_initiation_payment_create_request.amount = {
-      value: 100.00,
-      currency: 'GBP'
-    }
+    payment_initiation_payment_create_request = Plaid::PaymentInitiationPaymentCreateRequest.new(
+      {
+        recipient_id: recipient_id,
+        reference: 'testpayment',
+        amount: {
+          value: 100.00,
+          currency: 'GBP'
+        }
+      }
+    )
     create_payment_response = client.payment_initiation_payment_create(
       payment_initiation_payment_create_request
     )
     payment_id = create_payment_response.payment_id
 
-    link_token_create_request = Plaid::LinkTokenCreateRequest.new({
-      :user => { :client_user_id => 'user-id' },
-      :client_name => 'Plaid Quickstart',
-      :products => ENV['PLAID_PRODUCTS'].split(','),
-      :country_codes => ENV['PLAID_COUNTRY_CODES'].split(','),
-      :language => "en",
-      :payment_initiation=>{:payment_id=>payment_id},
-      :redirect_uri => nil_if_empty_envvar('PLAID_REDIRECT_URI')
-    })
+    link_token_create_request = Plaid::LinkTokenCreateRequest.new(
+      {
+        user: { client_user_id: 'user-id' },
+        client_name: 'Plaid Quickstart',
+        products: ENV['PLAID_PRODUCTS'].split(','),
+        country_codes: ENV['PLAID_COUNTRY_CODES'].split(','),
+        language: 'en',
+        payment_initiation: { payment_id: payment_id },
+        redirect_uri: nil_if_empty_envvar('PLAID_REDIRECT_URI')
+      }
+    )
     response = client.link_token_create(link_token_create_request)
     pretty_print_response(response)
     content_type :json
@@ -429,24 +446,4 @@ end
 
 def pretty_print_response(response)
   puts JSON.pretty_generate(response)
-end
-
-def poll_for_asset_report(asset_report_token)
-  response = nil
-  50.times do
-    begin
-      asset_report_get_request = Plaid::AssetReportGetRequest.new
-      asset_report_get_request.asset_report_token = asset_report_token
-
-      response = @client.asset_report_get(asset_report_get_request)
-      break
-    rescue Plaid::ApiError => e
-      json_response = JSON.parse(e.response_body)
-      raise e if json_response['error_code'] != 'PRODUCT_NOT_READY'
-      
-      sleep 1
-    end
-  end
-  assert response, 'Timed out while waiting for asset report generation'
-  response
 end
