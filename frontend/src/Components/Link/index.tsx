917246feda0e53e36e6c17332dc4808293b6c41b
Miles Song <msong@plaid.com>
2021-11-10 14:29:58 -0800

    fix frontend access token

--- frontend/src/Components/Link/index.tsx
+++ frontend/src/Components/Link/index.tsx
@@ -11,7 +11,7 @@ const Link = () => {
     (public_token: string) => {
       // send public_token to server
       const setToken = async () => {
-        const response = await fetch("/rout/To/TokenExchangeSaveToken/Function", {
+        const response = await fetch("/api/set_access_token", {
           method: "POST",
           headers: {
             "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
